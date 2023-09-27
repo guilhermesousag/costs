7c8df68b68edff14dd91e76225285e9dfc8068d5
Guilherme Sousa <guisg2001@hotmail.com>
2023-09-27 19:42:28 -0300

    New Commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 import Projects from './components/pages/Projects'
+import Project from './components/pages/Project'
 
 import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+
 
 function App() {
   return (
@@ -19,7 +21,8 @@ function App() {
             <Route path="/projects" element={<Projects />} />
             <Route path="/company" element={<Company />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/newproject" element={<NewProject />} />
+            <Route path='/newproject' element={<NewProject />} />
+            <Route path="/project/:id" element={<Project />} />
           </Routes>
         </Container>
         <Footer />
